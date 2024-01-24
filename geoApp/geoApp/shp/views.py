654777f41664ff296262cde594a3dbb62e7350e1
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-01-24 22:12:03 +0530

    updated the django project section

---
+++ geoApp/geoApp/shp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+# from .models import Shp
+# from tiff.models import Tiff
+# from note.models import Note
+
+
+def index(request):
+    # shp = Shp.objects.all()
+    # tiff = Tiff.objects.all()
+    # note = Note.objects.all()
+    return render(request, 'index.html')
+
+
+# def note(request):
+#     if(request.method == 'POST'):
+#         note_heading = request.POST.get('note-heading')
+#         note = request.POST.get('note')
+#         lat = request.POST.get('lat')
+#         lng = request.POST.get('lng')
+#         print(note_heading, note, lat, lng, 'email username')
+#         return render(request, 'index.html')
+#     return render(request, 'index.html')
